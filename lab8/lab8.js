77baadf40744fe2addea85e855c596dd12605e95
Nikolay <smir.nick2000@gmail.com>
2021-01-12 17:07:56 +0300

    Pimp your website - now for real

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -20,3 +20,20 @@ function SubmitReply() {
     replyTextInput.value = "";
 }
 replySubmitButton.addEventListener("click", SubmitReply);
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
